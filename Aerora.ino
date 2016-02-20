383e0ca8b3cc4b4315853674fd8f24d84a26bc36
mori3rti <borni3s.burn@gmail.com>
2016-02-20 20:58:09 +0700

    Add Aerora.ino. execute the project

---
+++ Aerora.ino
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+#define USING_AERORA
+#define TAHUN_2016
+
+
+
+
+
+
+
+
+
+
+
+#include "Aerora.h"
+
+void setup(){
+    RobotSetup();
+}
+void loop() {
+    RobotLoop();
+}
+
+
+
+
